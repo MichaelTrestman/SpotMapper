e388f0a9c70fdb57ac90e3befe26f7842ab704a8
Michael Trestman <michael.a.trestman@gmail.com>
2014-10-28 23:12:50 -0700

    some passing spots controller tests

--- spec/controllers/spots_controller_spec.rb
+++ spec/controllers/spots_controller_spec.rb
@@ -11,24 +11,24 @@ describe SpotsController do
   let(:spot_params) {
     FactoryGirl.attributes_for :spot
   }
-
-  context 'with valid spot params' do
-    it "creates a new spot"  do
-      post :create, list_id: 1, spot: spot_params
-      x = JSON.parse(response.body)
-      expect(
-        x["success"]
-        ).to eq( true )
+  describe "#create" do
+    context 'with valid spot params' do
+      it "creates a new spot"  do
+        post :create, list_id: 1, spot: spot_params
+        x = JSON.parse(response.body)
+        expect(
+          x["success"]
+          ).to eq( true )
+      end
+      it "creates a new spot"  do
+        expect(Spot.count).to eq 0
+        post :create, list_id: 1, spot: spot_params
+              x = JSON.parse(response.body)
+        expect(
+          x["success"]
+          ).to eq( true )
+        expect(Spot.count).to eq 1
+      end
     end
-    it "creates a new spot"  do
-      expect(Spot.count).to eq 0
-      post :create, list_id: 1, spot: spot_params
-            x = JSON.parse(response.body)
-      expect(
-        x["success"]
-        ).to eq( true )
-      expect(Spot.count).to eq 1
-    end
-
   end
 end
